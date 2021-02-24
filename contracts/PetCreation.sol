c30e17c133d924d9941d365e1b1629a51b8a908f
Scottburg <Scottb4545@gmail.com>
2021-02-24 16:58:45 +0000

    Fix: truffle connecting to tests

--- contracts/PetCreation.sol
+++ contracts/PetCreation.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.7.0;
-import '@openzeppelin/access/Ownable.sol';
+pragma solidity >=0.5.0 <0.8.0;
+// import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract Pet is Ownable {
+contract PetCreation  {
 
    event PetAdded(uint id, string name);
 
@@ -17,20 +17,20 @@ contract Pet is Ownable {
    Pet[] public pets;
    
 
-   function _addPet(string memory _name, uint _petId, string _breed) private {
-      require(_checkIdused(_petId));
-      pets.push(_name, _petId, msg.sender, false, '' , _breed);
-      emit PetAdded(_PetId, _name)
+   function _addPet(string memory _name, uint _petId, string memory _breed) public {
+      // require(_checkIdused(_petId));
+      // pets.push(_name, _petId, msg.sender, false, '' , _breed);
+      emit PetAdded(_petId, _name);
 
    }
 
-   function _checkIdUsed(uint _petId) private {
-      require (uint.length == 15);  // TEST this 
+   // function _checkIdUsed(uint _petId) private {
+   //    require (uint.length == 15);  // TEST this 
 
 
 
 
-   }
+   // }
 
 
 
--- test/PubPuppy.js
+++ test/PubPuppy.js
@@ -1,9 +1,18 @@
-const Pet = artifacts.require('Pet'); //artifacts creates a JS abstraction that allows you to test with mocha/chai syntax.
+const PetCreation = artifacts.require('PetCreation'); //artifacts creates a JS abstraction that allows you to test with mocha/chai syntax.
 //pet without .sol is fine as it checks the contracts based on the name #
 
-contract('Pet', (contracts) => {
-  let accounts = ['alice', 'bob', 'chuck'];
+contract('PetCreation', (accounts) => {
+  let [alice, bob, chuck] = accounts;
   it('should be able to create a new pet', async () => {
-    const contractInstance = await Pet.new();
+    const contractInstance = await PetCreation.new();
+    const res = await contractInstance._addPet(
+      'barkDog',
+      123456789012345,
+      'Wulf Hound',
+      { from: alice }
+    );
+    console.log(res);
+    assert.equal(res.receipt.status, true);
+    // assert.equal(res.logs[0], true);
   });
 });
